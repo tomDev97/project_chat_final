a71dae44e87fa47337c92b3c472130727cfcbd24
unknown <thanhtin581997@gmail.com>
2019-08-14 17:36:58 +0800

    connect db && create intance - setting env variable

---
+++ sh/env.example.sh
@@ -0,0 +1,11 @@
+# config db env
+export DB_CONNECTION=***
+export DB_HOST=***
+export DB_PORT=***
+export DB_NAME=***
+export DB_USERNAME=***
+export DB_PASSWORD=***
+
+# config app env
+export APP_HOST=***
+export APP_PORT=***
