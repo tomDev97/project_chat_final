d6e770bd3805257fe8f107927a68b1ad1a6eb371
unknown <thanhtin581997@gmail.com>
2019-08-15 15:55:23 +0800

    write route post register && validator

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,28 @@
+import { validationResult } from 'express-validator/check';
+
 let getLoginRegister = (req, res) => {
   return res.render('auth/master');
 };
 
+let postRegister = (req, res) => {
+  let errorArr = [];
+
+  let validatorErrors = validationResult(req);
+  if(!validatorErrors.isEmpty()) {
+    let errors = Object.values(validatorErrors.mapped());
+
+    errors.forEach(item => {
+      errorArr.push(item.msg);
+    });
+    console.log(errorArr);
+    return;
+  }
+  console.log(req.body);
+  
+}
+
+
 module.exports = {
-  getLoginRegister
+  getLoginRegister,
+  postRegister
 };
